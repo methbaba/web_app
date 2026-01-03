ff2d3171b144be2982b89a016b582f17a46aa428
chinmay thaokar <chinmaythaokar@gmail.com>
2026-01-03 19:50:21 +0530

    firstCommit

---
+++ react_app_2/src/App.jsx
@@ -0,0 +1,20 @@
+
+
+import Card from './Card.jsx'
+
+function App() {
+ 
+  return (
+
+    <>
+    <Card 
+    name="chinmay" age={25} isMale={false} isLogged={true}>
+    </Card>
+      
+    </>
+       
+  );
+  
+}
+
+export default App
